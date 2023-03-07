25f9bf978f85612536a789c0d1713a465f8b5f2d
Luciano Rojas Alonso <126501551+Lucianorojasalonso@users.noreply.github.com>
2023-03-07 20:31:00 -0300

    Delete tipo-de-variables.js

--- tipo-de-variables.js
+++
@@ -1,8 +0,0 @@
-
-
-let numero;       // Definimos la variable x
-console.log (numero);
-numero = 5;       // le asignamos un valor numerico
-console.log (numero);
-let nombre = "John";  // x ahora alcanza un string
-console.log (nombre);
